00130a962da55a294d147ec6510f2116090d4858
quk1se <alexandrkalyan953@gmail.com>
2024-03-31 21:25:36 +0300

    firebase

---
+++ SeaGateTest/Controllers/LoginViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LoginViewController.swift
+//  SeaGateTest
+//
+//  Created by moseu on 31.03.2024.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
---
+++ SeaGateTest/Controllers/RegisterViewController.swift
@@ -0,0 +1,18 @@
+//
+//  RegisterViewController.swift
+//  SeaGateTest
+//
+//  Created by moseu on 31.03.2024.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
